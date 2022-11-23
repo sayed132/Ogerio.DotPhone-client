47f66791561be1a7afb661e5cb5a3140b73e92a8
Abu Sayed <rafsansayed132@gmail.com>
2022-11-24 02:46:31 +0600

    added extra section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import About from '../About/About';
 import Banner from '../Banner/Banner'
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <About></About>
         </div>
     );
 };
