0282896782cf4129136ebcdb707d04e6932923ee
Abu Sayed <rafsansayed132@gmail.com>
2022-11-24 17:13:57 +0600

    added signup

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,9 +1,119 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
 
 const Signup = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { createUser, updateUserProfile } = useContext(AuthContext);
+    const [signUpError, setSignUPError] = useState('')
+    // const [createdUserEmail, setCreatedUserEmail] = useState('')
+    // const [token] = useToken(createdUserEmail);
+    const navigate = useNavigate();
+
+    // if(token){
+
+    // }
+    const handleSignUp = (data) => {
+        console.log(data);
+        setSignUPError('');
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                const name = data?.name
+                const photoURL = data?.photoURL;
+                const selected = data?.selected;
+                console.log(user, selected);
+                toast.success('User Created Successfully.');
+                navigate('/');
+                handleUpdateUserProfile(name, photoURL)
+                saveUser(data.name, data.email, selected)
+            })
+            .catch(error => {
+                console.log(error)
+                setSignUPError(error.message)
+            });
+    }
+
+    const handleUpdateUserProfile = (name, photoURL) => {
+        const profile = {
+            displayName: name,
+            photoURL: photoURL
+        }
+
+        updateUserProfile(profile)
+            .then(() => {
+
+            })
+            .catch(error => console.error(error));
+    }
+
+    const saveUser = (name, email, selected) => {
+        const user = { name, email ,selected };
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('save user', data);
+                // setCreatedUserEmail(email);
+            })
+    }
+
+
     return (
-        <div>
-            <h2>signup</h2>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center'>Sign Up</h2>
+                <form onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Name</span></label>
+                        <input type="text" {...register("name", {
+                            required: "Name is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">photoURL</span></label>
+                        <input type="text" {...register("photoURL", {
+                            required: "photoURL is required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="email" {...register("email", {
+                            required: true
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type="password" {...register("password", {
+                            required: "Password is required",
+                            minLength: { value: 6, message: "Password must be 6 characters long" },
+                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs my-4 font-semibold">
+                        <select className="select select-info w-full max-w-xs" {...register("selected", {
+                            required: "account choose is Required"
+                        })}>
+                            <option >Buyer Account</option>
+                            <option >Seller Account</option>
+                        </select>
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                    </div>
+                    <input className='btn btn-accent w-full mt-4' value="Sign Up" type="submit" />
+                    {signUpError && <p className='text-red-600'>{signUpError}</p>}
+                </form>
+                <p>Already have an account <Link className='text-secondary' to="/login">Please Login</Link></p>
+            </div>
         </div>
     );
 };
